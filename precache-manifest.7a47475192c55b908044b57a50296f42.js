87820284adc515777890c2959d1c4f9454b246ba
PrakashSharma2000 <ps4687675@gmail.com>
2020-10-20 21:00:13 +0530

    Updates

---
+++ precache-manifest.7a47475192c55b908044b57a50296f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c21c5df6afc4e9f69fe2d095bdba39e",
+    "url": "/Deadpoolfanpage/index.html"
+  },
+  {
+    "revision": "44a562c85f362a3b1bd8",
+    "url": "/Deadpoolfanpage/static/css/main.9cd882ed.chunk.css"
+  },
+  {
+    "revision": "92714c83708cc0407382",
+    "url": "/Deadpoolfanpage/static/js/2.162a38ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Deadpoolfanpage/static/js/2.162a38ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a562c85f362a3b1bd8",
+    "url": "/Deadpoolfanpage/static/js/main.fd32d1e0.chunk.js"
+  },
+  {
+    "revision": "434563bc4f91dadddbc6",
+    "url": "/Deadpoolfanpage/static/js/runtime-main.c8c12a93.js"
+  }
+]);
